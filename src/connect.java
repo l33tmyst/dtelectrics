60d1d4531215f752106d512fd069ebba23fd3dd0
Harry Rickards <hrickards@l33tmyst.com>
2009-06-13 10:15:58 +0100

    Added a list of variables (the same as the in the database)
    Added some code that adds the contents of these variables to the database.
    
    Tested it and it works great. Now need to link up the form code and this code.

--- src/connect.java
+++ src/connect.java
@@ -7,15 +7,36 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import com.mysql.jdbc.Statement;
-
-
 
 
    public class connect
    {
+	   
+	   static String title = "Mr";
+	   static String forename = "Harry";
+	   static String surname = "Rickards";
+	   static String address0 = "105 Parklands";
+	   static String address1 = "Maresfield";
+	   static String address2 = "";
+	   static String address3 = "";
+	   static String address4 = "";
+	   static String address5 = "";
+	   static String postcode = "TN22 2HT";
+	   static String phone = "01825760629";
+	   static String email = "hrickards@l33tmyst.com";
+	   static String type = "Example type";
+	   static String fax = "";
+	   static String mobile = "";
+	   
+	   static java.sql.Statement stmt;
+
+	      
+	   static String insertString = "insert into customers values(1, '"+title+"', '"+forename+"', '"+surname+"', '"+address0+"', '"+address1+"', '"+address2+"', '"+address3+"', '"+address4+"', '"+address5+"', '"+postcode+"', '"+phone+"', '"+email+"', '"+type+"', '"+fax+"', '"+mobile+"')";
+	   
        public static void main (String[] args)
        {
+    	   
+    	   System.out.println(insertString);
            Connection con = null;
 
            try
@@ -31,7 +52,9 @@ import com.mysql.jdbc.Statement;
 
         	     // Do something with the Connection
 
-               
+               stmt = con.createStatement();
+   	   		stmt.executeUpdate(insertString);
+
             
                  
            } catch (SQLException ex) {
