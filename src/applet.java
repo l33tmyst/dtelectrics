be515d69318521ca51ec7effa422c01eca036d19
l33tmyst <hrickards@l33tmyst.com>
2009-08-12 04:00:46 -0700

    Added validation FINALLY

--- src/applet.java
+++ src/applet.java
@@ -465,160 +465,248 @@ public class applet extends Applet  {
 
 	//Sets up the standard class database()
 	public void database() {
+		
+			int valid = 0;
+			String forename = null, surname = null, address0 = null, address1 = null;
+			String address2 = null, address3 = null, address4 = null, address5 = null;
+			String postcode = null, phone = null, email = null, fax = null, mobile = null;
+			
 			//Creates a string called title that get's the selected item from jComboBox2
 			String title = (String) jComboBox2.getSelectedItem();
 			
-			//Creates a string called forename that get's the text from jTextField0
-			String forename = jTextField0.getText();
+
+			if (jTextField0.getText().equals(""))
+			{
+				jLabel1.setForeground(Color.red);
+				valid = 1;
+			} else {
+				jLabel1.setForeground(Color.black);
+				//Creates a string called forename that get's the text from jTextField0
+				forename = jTextField0.getText();
+			}
 			
-			//Creates a string called surname that get's the text from jTextField1
-			String surname = jTextField1.getText();
+			if (jTextField1.getText().equals(""))
+			{
+				jLabel2.setForeground(Color.red);
+				valid = 1;
+			} else {
+				jLabel2.setForeground(Color.black);
+				//Creates a string called surname that get's the text from jTextField1
+				surname = jTextField1.getText();
+			}
 			
-			//Creates a string called address0 that get's the text from jTextField2
-			String address0 = jTextField2.getText();
+			if (jTextField2.getText().equals(""))
+			{
+				jLabel3.setForeground(Color.red);
+				valid = 1;
+			} else {
+				jLabel3.setForeground(Color.black);
+				//Creates a string called address0 that get's the text from jTextField2
+				address0 = jTextField2.getText();
+			}
 			
 			//Creates a string called address1 that get's the text from jTextField3
-			String address1 = jTextField3.getText();
-			
+			address1 = jTextField3.getText();
+
 			//Creates a string called address2 that get's the text from jTextField4
-			String address2 = jTextField4.getText();
-			
+			address2 = jTextField4.getText();
+		
 			//Creates a string called address3 that get's the text from jTextField5
-			String address3 = jTextField5.getText();
-			
+			address3 = jTextField5.getText();
+						
 			//Creates a string called address4 that get's the text from jTextField6
-			String address4 = jTextField6.getText();
-			
+			address4 = jTextField6.getText();
+					
 			//Creates a string called address5 that get's the text from jTextField7
-			String address5 = jTextField7.getText();
+			address5 = jTextField7.getText();
+						
+			if (jTextField8.getText().equals("") || jTextField8.getText().length() <6 || jTextField8.getText().length() >8)
+			{
+				jLabel14.setForeground(Color.red);
+				valid = 1;
+			} else {
+				if (jTextField8.getText().matches("[A-Z]{1,2}[1-9][0-9]? ?[0-9][A-Z]{2}"))
+				{
+					jLabel14.setForeground(Color.black);
+					//Creates a string called postcode that get's the text from jTextField8
+					postcode = jTextField8.getText();					
+				} else {
+					jLabel14.setForeground(Color.red);
+					valid = 1;					
+				}
+			}
 			
-			//Creates a string called postcode that get's the text from jTextField8
-			String postcode = jTextField8.getText();
 			
-			//Creates a string called phone that get's the text from jTextField9
-			String phone = jTextField9.getText();
+			if (jTextField9.getText().matches("^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{5})$"))
+			{
+				jLabel9.setForeground(Color.black);
+				//Creates a string called phone that get's the text from jTextField9
+				phone = jTextField9.getText();
+			} else if (jTextField9.getText().equals(""))
+			{
+				jLabel9.setForeground(Color.black);
+				//Creates a string called phone that get's the text from jTextField9
+				phone = jTextField9.getText();
+			} else {
+				jLabel9.setForeground(Color.red);
+				valid = 1;
+			}
 			
-			//Creates a string called email that get's the text from jTextField10
-			String email = jTextField10.getText();
+			if (jTextField10.getText().equals("") || jTextField10.getText().length() < 7)
+			{
+				jLabel10.setForeground(Color.red);
+				valid = 1;
+			} else {
+					jLabel10.setForeground(Color.black);
+					//Creates a string called email that get's the text from jTextField10
+					email = jTextField10.getText();
+			}
 			
 			//Creates a string called type that get's the selected item from jComboBox1
 			String type = (String) jComboBox1.getSelectedItem();
 			
-			//Creates a string called fax that get's the text from jTextField11
-			String fax = jTextField11.getText();
+			if (jTextField11.getText().matches("^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{5})$"))
+			{
+				jLabel12.setForeground(Color.black);
+				//Creates a string called fax that get's the text from jTextField11
+				fax = jTextField11.getText();
+			} else if (jTextField11.getText().equals(""))
+			{
+				jLabel12.setForeground(Color.black);
+				//Creates a string called fax that get's the text from jTextField11
+				fax = jTextField11.getText();
+			} else {
+				jLabel12.setForeground(Color.red);
+				valid = 1;
+			}
 			
-			//Creates a string called mobile that get's the text from jTextField12
-			String mobile = jTextField12.getText();
+			if (jTextField12.getText().equals("^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{5})$"))
+			{
+				jLabel13.setForeground(Color.black);
+				//Creates a string called mobile that get's the text from jTextField12
+				mobile = jTextField12.getText();
+			} else if (jTextField12.getText().equals(""))
+			{
+				jLabel13.setForeground(Color.black);
+				//Creates a string called mobile that get's the text from jTextField12
+				mobile = jTextField12.getText();
+			} else {
+				jLabel13.setForeground(Color.red);
+				valid = 1;
+			}
 			
-			//Creates a string called insertString that will insert the variables (see above)
-			//into the relevant fields in the MySQL table
-			String insertString = "insert into customers (title, forename, surname, address0, address1, address2, address3, address4, address5, postcode, phone, email, type, fax, mobile) values('"+title+"', '"+forename+"', '"+surname+"', '"+address0+"', '"+address1+"', '"+address2+"', '"+address3+"', '"+address4+"', '"+address5+"', '"+postcode+"', '"+phone+"', '"+email+"', '"+type+"', '"+fax+"', '"+mobile+"')";
+			if (valid == 0)
+			{
+				//Creates a string called insertString that will insert the variables (see above)
+				//into the relevant fields in the MySQL table
+				String insertString = "insert into customers (title, forename, surname, address0, address1, address2, address3, address4, address5, postcode, phone, email, type, fax, mobile) values('"+title+"', '"+forename+"', '"+surname+"', '"+address0+"', '"+address1+"', '"+address2+"', '"+address3+"', '"+address4+"', '"+address5+"', '"+postcode+"', '"+phone+"', '"+email+"', '"+type+"', '"+fax+"', '"+mobile+"')";
 			
-			//Uses Statement from java.sql to create a statement called stmt
-			//Needs to be directly referenced as java.sql.Statement to avoid confusion with
-			//java.beans.Statement
-			java.sql.Statement stmt;
+				//Uses Statement from java.sql to create a statement called stmt
+				//Needs to be directly referenced as java.sql.Statement to avoid confusion with
+				//java.beans.Statement
+				java.sql.Statement stmt;
 			
-			//Uses Connection to create a null connection called con
-			Connection con = null;
+				//Uses Connection to create a null connection called con
+				Connection con = null;
+			
+					try
+					{
 
-	           try
-	           {
+						//Creates a new JDBC instance
+						Class.forName ("com.mysql.jdbc.Driver").newInstance ();
+	        	   	
+						/*
+						 * IMPORTANT: For obvious reasons the following variables such as
+						 * mydatabase, username and password have been changed so that an
+						 * attacker can't fill up the database with meaningless entries.	        	   	
+						 */
+	        	   	
+	        	   	
+						//Creates a variable called serverName with the value of localhost
+						//This will set the server name as localhost
+						//Needs to be changed in a production environment
+						String serverName = "localhost";
+	        	   	
+						//Creates a variable called mydatabase with the value of dtelectrics
+						//This will set the name of the database to dtelectrics
+						//Needs to be changed in a production environment
+						String mydatabase = "dtelectrics";
+	        	   	
+						//Creates a variable called url that combines the previous along with
+						//some other info
+						String url = "jdbc:mysql://" + serverName +  "/" + mydatabase;
+	        	   	
+						//Creates a variable called username with the value of dtelectrics
+						//This will set the username to log in as to dtelectrics
+						//Needs to be changed in a production environment
+						String username = "dtelectrics";
+	        	   	
+						//Creates a variable called password with the value of cr1ppl34t0r
+						//This will set the password to log in with to cr1ppl34t0r
+						//Needs to be changed in a production environment
+						String password = "passw0rd";
+	        	   	
+						//Uses the previously created connection con to connect to the database
+						//using the variables url, username and password
+						con = DriverManager.getConnection(url, username, password);
 
-	        	   	//Creates a new JDBC instance
-	        	   	Class.forName ("com.mysql.jdbc.Driver").newInstance ();
+						//The previously created statement stmt uses the previously created
+						//connection con to create a statement
+						stmt = con.createStatement();
 	        	   	
-	        	   	/*
-	        	   	 * IMPORTANT: For obvious reasons the following variables such as
-	        	   	 * mydatabase, username and password have been changed so that an
-	        	   	 * attacker can't fill up the database with meaningless entries.	        	   	
-	        	   	 */
-	        	   	
-	        	   	
-	        	   	//Creates a variable called serverName with the value of localhost
-	        	   	//This will set the server name as localhost
-	        	   	//Needs to be changed in a production environment
-	        	   	String serverName = "localhost";
-	        	   	
-	        	   	//Creates a variable called mydatabase with the value of dtelectrics
-	        	   	//This will set the name of the database to dtelectrics
-	        	   	//Needs to be changed in a production environment
-	        	   	String mydatabase = "dtelectrics";
-	        	   	
-	        	   	//Creates a variable called url that combines the previous along with
-	        	   	//some other info
-	        	   	String url = "jdbc:mysql://" + serverName +  "/" + mydatabase;
-	        	   	
-	        	   	//Creates a variable called username with the value of dtelectrics
-	        	   	//This will set the username to log in as to dtelectrics
-	        	   	//Needs to be changed in a production environment
-	        	   	String username = "dtelectrics";
-	        	   	
-	        	   	//Creates a variable called password with the value of cr1ppl34t0r
-	        	   	//This will set the password to log in with to cr1ppl34t0r
-	        	   	//Needs to be changed in a production environment
-	        	   	String password = "cr1ppl34t0r";
-	        	   	
-	        	   	//Uses the previously created connection con to connect to the database
-	        	   	//using the variables url, username and password
-	        	   	con = DriverManager.getConnection(url, username, password);
-
-	        	   	//The previously created statement stmt uses the previously created
-	        	   	//connection con to create a statement
-	        	   	stmt = con.createStatement();
-	        	   	
-	        	   	//stmt then runs the SQL command in the variable insertString
-	        	   	stmt.executeUpdate(insertString);
+		        	   	//stmt then runs the SQL command in the variable insertString
+		        	   	stmt.executeUpdate(insertString);
 
 	            
-	           //Checks for any SQL errors
-	           } catch (SQLException ex) {
-	        	    //If there are errors prints out the following debugging info:
-	        	   	//SQL Exception number, SQL State and Vendor Error Number
-	        	   	System.out.println("SQLException: " + ex.getMessage());
-	        	    System.out.println("SQLState: " + ex.getSQLState());
-	        	    System.out.println("VendorError: " + ex.getErrorCode());
+		        	   	//Checks for any SQL errors
+					} catch (SQLException ex) {
+						//If there are errors prints out the following debugging info:
+		        	   	//SQL Exception number, SQL State and Vendor Error Number
+		        	   	System.out.println("SQLException: " + ex.getMessage());
+		        	   	System.out.println("SQLState: " + ex.getSQLState());
+		        	   	System.out.println("VendorError: " + ex.getErrorCode());
 	        	
-	        	//Checks for any other errors
-	           } catch (Exception e) {
-	        	   //Prints out an error message informing the user about the unknown error
-	        	   //Clarification: Will show up in the Java console not the form
-	               System.err.println ("UNKOWN ERROR: Cann't connect to database");
+		        	   	//Checks for any other errors
+					} catch (Exception e) {
+						//Prints out an error message informing the user about the unknown error
+						//Clarification: Will show up in the Java console not the form
+						System.err.println ("UNKOWN ERROR: Cann't connect to database");
 	           
 	               
                 
 
 	           
-	           } finally {
-	        	   //If connection isn't null
-	        	   //(although confusing means connection is finished)
-	        	   if (con != null)
-	               {
-	                   try
-	                   {
-	                	   //Close the connection
-	                	   con.close ();
-	                	   
-	                	   //Print out a message to the Java console saying that the
-	                	   //connection has been disconnected
-	                       System.out.println ("Disconnected from database");
+					} finally {
+						//If connection isn't null
+						//(although confusing means connection is finished)
+						if (con != null)
+						{
+							try
+							{
+								//Close the connection
+								con.close ();
+								
+								//Print out a message to the Java console saying that the
+								//connection has been disconnected
+								System.out.println ("Disconnected from database");
 	                       
-	    	               //Increases the length of jButton so more text can fit
-	    	               jButton.setSize(90, 25);               
+								//Increases the length of jButton so more text can fit
+								jButton.setSize(90, 25);               
 	    	               
-	    	               //Sets the text of jButton to thanks
-	    	               jButton.setText("Thanks");	       
+								//Sets the text of jButton to thanks
+								jButton.setText("Thanks");	       
 	    	               
-	    	               //Sets the text colour of jButton to red
-	    	               jButton.setForeground(Color.red);
-	                   }
-	                   catch (Exception e) {
-	                	   /* Ignores any errors that happen when
-	                   		*closing the connection
-	                	    */ 
+								//Sets the text colour of jButton to red
+								jButton.setForeground(Color.red);
+							}
+							catch (Exception e) {
+								/* Ignores any errors that happen when
+								 *closing the connection
+								 */ 
 	                	   }
 	               }
 	           }
+			}
 	}
 	
 
